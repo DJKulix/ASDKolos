570582c38fb94bf0378673191328b380a7b2fbba
DJKulix <mkula737@gmail.com>
2022-02-07 14:07:40 +0100

    Add files via upload

--- LodyMonteCarlo.java
+++ LodyMonteCarlo.java
@@ -1,94 +1,69 @@
 package com.company;
 
-import java.util.Arrays;
+import java.util.Random;
 
 public class LodyMonteCarlo {
 
-    final static int N = 6;
-    public static int minTime = Integer.MAX_VALUE;
-    public static int currentTime = 0;
-    public static int[][] matrix = new int[6][6];
-    private static final int[] permutacja = new int[N];
+    final static int N = 10000;
+    private static int[][] tab =
+            {{0, 7, 20, 21, 12, 23},
+                    {27, 0, 13, 16, 46, 5},
+                    {53, 15, 0, 25, 27, 6},
+                    {16, 2, 35, 0, 47, 10},
+                    {31, 29, 5, 18, 0, 4},
+                    {28, 24, 1, 17, 5, 0}};
+    private static int[] storage = new int[7];
+    private static int bestSolution = 10000000;
+    private static int currentSolution;
+    private static int check;
+    private static int timeCost = 0;
 
     public static void main(String[] args) {
+        Random random = new Random();
 
-        matrix[0][0] = 0;
-        matrix[0][1] = 7;
-        matrix[0][2] = 20;
-        matrix[0][3] = 21;
-        matrix[0][4] = 12;
-        matrix[0][5] = 23;
-
-        matrix[1][0] = 27;
-        matrix[1][1] = 0;
-        matrix[1][2] = 13;
-        matrix[1][3] = 16;
-        matrix[1][4] = 46;
-        matrix[1][5] = 5;
-
-        matrix[2][0] = 53;
-        matrix[2][1] = 15;
-        matrix[2][2] = 0;
-        matrix[2][3] = 25;
-        matrix[2][4] = 27;
-        matrix[2][5] = 6;
-
-        matrix[3][0] = 16;
-        matrix[3][1] = 2;
-        matrix[3][2] = 35;
-        matrix[3][3] = 0;
-        matrix[3][4] = 47;
-        matrix[3][5] = 10;
-
-        matrix[4][0] = 31;
-        matrix[4][1] = 29;
-        matrix[4][2] = 5;
-        matrix[4][3] = 18;
-        matrix[4][4] = 0;
-        matrix[4][5] = 4;
-
-        matrix[5][0] = 28;
-        matrix[5][1] = 24;
-        matrix[5][2] = 1;
-        matrix[5][3] = 17;
-        matrix[5][4] = 5;
-        matrix[5][5] = 0;
-
-        permutacje(0);
-
-    }
-
-    static void permutacje(int i) {
-
-        if (i == N) {
-            for (int k = 0; k < N; k++) {
-                currentTime += matrix[permutacja[(k % N)] - 1][permutacja[(k + 1) % N] - 1];
+        for (int i = 0; i < N; i++) {
+            for (int a = 0; a < 7; a++) {
+                storage[a] = random.nextInt(6) + 1;
             }
-            if (currentTime <= minTime) {
-                minTime = currentTime;
-                System.out.println("Czas:\t\tKolejność:");
-                System.out.print(minTime + "\t\t");
-                for (int j = 0; j < N + 1; j++) {
-                    System.out.print(permutacja[j % N] + " ");
-                }
-                System.out.println("\n");
+            storage[6] = storage[0];
+
+            for (int j = 0; j < 6; j++) {
+                timeCost = tab[storage[j] - 1][storage[j + 1] - 1] + timeCost;
             }
 
-            currentTime = 0;
-        }
-        else {
-            for (int j = 1; j <= N; j++) {
-                int k;
-                for (k = 0; k < i; k++) {
-                    if (permutacja[k] == j) {
+            timeCost = timeCost + tab[storage[6] - 1][storage[0] - 1];
+            currentSolution = timeCost;
+            timeCost = 0;
+
+            int q = 0;
+            for (int k = 0; k < 7; k++) {
+                int temp = k + 1;
+                while (temp < 7) {
+                    if (storage[k] == storage[temp]) {
+                        q++;
                         break;
                     }
-                }
-                if (k == i) {
-                    permutacja[k] = j;
-                    permutacje(i + 1);
+                    temp++;
                 }
             }
+
+            if (currentSolution < bestSolution && q == 1) {
+                System.out.print("Smaki: ");
+                for (int a = 0; a < 7; a++) {
+                    System.out.print(storage[a]);
+                }
+                bestSolution = currentSolution;
+                System.out.println();
+                System.out.println("Koszt: " + currentSolution);
+                check = 1;
+                System.out.println();
+            }
+            currentSolution = 0;
+        }
+        if (check != 1) {
+            System.out.println("Nie znaleziono rozwiazania!");
         }
     }
 }
+
+
